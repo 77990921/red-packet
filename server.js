fe0cfef2406672f8ccee7bbbe1e27a360a2e801f
77990921 <151946037+77990921@users.noreply.github.com>
2025-01-25 14:27:41 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 // 修改ComfyUI服务器地址
 const COMFY_API = process.env.NODE_ENV === 'production' 
-    ? 'http://127.0.0.1:7000'  // 通过7000端口的代理服务访问
+    ? 'http://127.0.0.1:6006/v2'  // 通过代理访问ComfyUI，使用v2前缀
     : 'http://127.0.0.1:6006';
 
 const app = express();
